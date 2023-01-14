21e26c8bdcbbb2dc65d66e28ebcc564ac88744e8
BobbyGaDouche <72995336+BobbyGaDouche@users.noreply.github.com>
2023-01-14 13:23:37 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const request = require('request');
+const express = require("express");
+const cors = require("cors");
+const request = require("request");
 const app = express();
-const port = process.env.PORT || 3000;
+
+app.use(cors({ origin: "https://brewed.online" }));
 app.get('/', (req, res, next) => {
 
     res.status(200).json({
@@ -13,29 +15,36 @@ app.get('/', (req, res, next) => {
     });
 
 });
+app.put("/update-product-tags", (req, res) => {
+  const accessToken = "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";
+  const productId = req.body.productId;
+  const tags = req.body.tags;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.put('/tag-product', (req, res) => {
-  const { productId, accessToken, tags } = req.body;
-  const url = `https://your-store.myshopify.com/admin/api/2021-01/products/${productId}/tags.json`;
-
-  request.put({
-    url,
+  const options = {
+    method: "PUT",
+    url: `https://www.brewed.online/admin/api/2021-01/products/${productId}/tags.json`,
     headers: {
-      'Content-Type': 'application/json',
-      'X-Shopify-Access-Token': accessToken
+      "Content-Type": "application/json",
+      "X-Shopify-Access-Token": accessToken
     },
-    body: JSON.stringify({ tags })
-  }, (err, httpResponse, body) => {
-    if (err) {
-      return res.status(500).send(err);
+    body: JSON.stringify({ tags: tags })
+  };
+
+  request(options, (error, response) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.status(200).send(response);
     }
-    return res.status(200).send(body);
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
+
+
+
+
+
+
