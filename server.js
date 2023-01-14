442e3ec2354ae69a4329cf6bbd2546a5945899d7
BobbyGaDouche <72995336+BobbyGaDouche@users.noreply.github.com>
2023-01-14 13:53:22 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,40 @@
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            name: 'name of your app',
-            version: '0.1.0'
-        }
-    });
-
-});
 const express = require('express');
 const request = require('request');
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.put('/tag-product', (req, res) => {
-  const { productId, accessToken, tags } = req.body;
-  const url = `https://your-store.myshopify.com/admin/api/2021-01/products/${productId}/tags.json`;
+app.put('/update-tags', (req, res) => {
+    const accessToken = req.headers['x-shopify-access-token'];
+    const shop = req.headers['x-shopify-shop-domain'];
+    const productId = req.body.productId;
+    const newTags = req.body.tags;
 
-  request.put({
-    url,
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Shopify-Access-Token': accessToken
-    },
-    body: JSON.stringify({ tags })
-  }, (err, httpResponse, body) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    return res.status(200).send(body);
-  });
+    const options = {
+        method: 'PUT',
+        url: `https://${shop}/admin/api/2021-01/products/${productId}/tags.json`,
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Shopify-Access-Token': accessToken
+        },
+        json: {
+            tags: newTags
+        }
+    };
+
+    request(options, (error, response, body) => {
+        if (error) {
+            return res.status(500).send(error.message);
+        }
+
+        if (response.statusCode !== 200) {
+            return res.status(response.statusCode).send(body);
+        }
+
+        res.status(200).send(body);
+    });
 });
 
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server running on port 3000');
 });
